0473162e1fab7c68773c4c9376c5de99c87d70b4
Aleksandr A Serdyukov <serdukovaa@yandex.ru>
2024-06-15 09:35:48 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.68")
+    implementation("org.bitcoinj:bitcoinj-core:0.15.10")
+    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.5.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.21")
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/MainOff.kt
@@ -0,0 +1,104 @@
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.security.MessageDigest
+import java.security.SecureRandom
+import java.util.concurrent.Executors
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.thread
+import org.bouncycastle.jce.ECNamedCurveTable
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.util.encoders.Hex
+import java.security.Security
+import java.security.KeyPairGenerator
+import java.security.spec.ECGenParameterSpec
+import org.bouncycastle.jce.interfaces.ECPrivateKey
+import org.bouncycastle.jce.interfaces.ECPublicKey
+import org.bouncycastle.crypto.digests.RIPEMD160Digest
+import org.bitcoinj.core.Base58
+
+fun generateKeyAndAddress(): Pair<String, String> {
+    Security.addProvider(BouncyCastleProvider())
+    val ecSpec = ECNamedCurveTable.getParameterSpec("P-256")
+    val g = KeyPairGenerator.getInstance("ECDSA", "BC")
+    g.initialize(ECGenParameterSpec("P-256"), SecureRandom())
+
+    val pair = g.generateKeyPair()
+    val privateKey = pair.private as ECPrivateKey
+    val publicKey = pair.public as ECPublicKey
+
+    val publicKeyBytes = publicKey.q.getEncoded(false).copyOfRange(1, 65)
+    val address = publicKeyToAddress(publicKeyBytes)
+
+    return Pair(Hex.toHexString(privateKey.d.toByteArray()), address)
+}
+
+fun publicKeyToAddress(publicKeyBytes: ByteArray): String {
+    val sha256 = MessageDigest.getInstance("SHA-256").digest(publicKeyBytes)
+    val ripemd160 = RIPEMD160Digest()
+    ripemd160.update(sha256, 0, sha256.size)
+    val ripemd160Result = ByteArray(ripemd160.digestSize)
+    ripemd160.doFinal(ripemd160Result, 0)
+
+    val networkVersion = byteArrayOf(0x00)
+    val addressBytes = networkVersion + ripemd160Result
+    val checksum = sha256Checksum(addressBytes)
+    val fullAddress = addressBytes + checksum
+
+    return Base58.encode(fullAddress)
+}
+
+fun sha256Checksum(input: ByteArray): ByteArray {
+    val firstSHA = MessageDigest.getInstance("SHA-256").digest(input)
+    val secondSHA = MessageDigest.getInstance("SHA-256").digest(firstSHA)
+    return secondSHA.copyOfRange(0, 4)
+}
+
+fun worker(id: Int, outputFile: String, btcAddresses: Map<String, Double>, lock: ReentrantLock) {
+    while (true) {
+        val (privateKey, publicAddress) = generateKeyAndAddress()
+         println("($id) Publicaddress: $publicAddress")
+
+         if(btcAddresses.containsKey(publicAddress)){
+             println("($id) Match Found! Privatekey: $privateKey Publicaddress: $publicAddress Balance: ${btcAddresses.get(publicAddress)}")
+             lock.lock()
+             try {
+                 File(outputFile).appendText("$privateKey:$publicAddress\n")
+             } finally {
+                 lock.unlock()
+             }
+         }
+
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val numThreads = Runtime.getRuntime().availableProcessors()
+    val outputFile = "out.txt"
+
+    val btcAddresses = parseCsvToMap("Latest_Rich_P2SH_Bitcoin_Address_Balance.csv") ///https://github.com/Pymmdrza/Rich-Address-Wallet/tree/main
+
+    val lock = ReentrantLock()
+    val threadPool = Executors.newFixedThreadPool(numThreads)
+
+    repeat(numThreads) { id ->
+        threadPool.submit { worker(id, outputFile, btcAddresses, lock) }
+    }
+
+    threadPool.shutdown()
+}
+
+
+fun parseCsvToMap(filePath: String): Map<String, Double> {
+    val addressBalanceMap = mutableMapOf<String, Double>()
+    File(filePath).useLines { lines ->
+        lines.drop(1) // Пропускаем заголовок
+            .forEach { line ->
+                val (address, balance) = line.split(",")
+                addressBalanceMap[address] = balance.toDouble()
+            }
+    }
+    return addressBalanceMap
+}
+
